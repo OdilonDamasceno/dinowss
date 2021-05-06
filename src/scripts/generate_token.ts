f674710ff732da2205d3d55025c495463c15474f
Odilon Damasceno <storckskey@gmail.com>
2021-05-06 19:05:07 -0300

    test: get enviroment from desktop

--- src/scripts/generate_token.ts
+++ src/scripts/generate_token.ts
@@ -1,5 +1,10 @@
 import { create, env } from "../../deps.ts";
+const enviroment = Deno.env;
 
 export default async function generateToken(params = {}) {
-  return await create({ alg: "HS512", typ: "JWT" }, params, env.JWT);
+  return await create(
+    { alg: "HS512", typ: "JWT" },
+    params,
+    env.JWT ?? enviroment.get("JWT"),
+  );
 }
--- tests/token.test.ts
+++ tests/token.test.ts
@@ -1,9 +1,14 @@
 import { assertEquals, env, verify } from "../deps.ts";
 import generateToken from "../src/scripts/generate_token.ts";
+const enviroment = Deno.env;
 
 Deno.test("jwt", async () => {
   const token = await generateToken({ _id: "1234" });
   assertEquals<string>(typeof token, "string", "Type of token is string!");
-  const promise = await verify(token, env.JWT, "HS512");
+  const promise = await verify(
+    token,
+    env.JWT ?? enviroment.get("JWT"),
+    "HS512",
+  );
   assertEquals(promise._id, "1234", "Token verified!");
 });
