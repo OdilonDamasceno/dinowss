888cc9d48c5530401e745700978f7d9681ae6825
Odilon Damasceno <storckskey@gmail.com>
2021-05-06 12:19:14 -0300

    feat: add mongodb var to enviroment

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from "../../deps.ts";
 import { UserSchema } from "./models/User.ts";
+import { env } from "../../deps.ts";
 
 const client = new MongoClient();
-await client.connect("mongodb://172.17.0.2:27017:27017");
+await client.connect(env.MONGODB);
 
 const db = client.database("dinodb");
 export const users = db.collection<UserSchema>("users");
